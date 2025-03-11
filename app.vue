81e73d4cbd3bc321efec1236012d0acd6bcd3e5a
teleskop150750 <teleskop150750@gmail.com>
2025-03-11 11:35:16 +0300

    wip

--- app.vue
+++ app.vue
@@ -1,14 +1,26 @@
+<script setup lang="ts">
+const queryCache = useQueryCache();
+
+function onClick() {
+  queryCache.invalidateQueries({ key: ["todos"] });
+}
+</script>
+
 <template>
   <div>
     <div>
       <nuxt-link to="/">Home</nuxt-link>
-      <nuxt-link to="/lazy">Lazy</nuxt-link>
-      <nuxt-link to="/self">Self</nuxt-link>
+      <nuxt-link to="/one">One</nuxt-link>
+      <nuxt-link to="/two">Two</nuxt-link>
+      <nuxt-link to="/three">Three</nuxt-link>
     </div>
     <div>
       <NuxtLayout>
         <NuxtPage />
       </NuxtLayout>
+    </div>
+    <div>
+      <button type="button" @click="onClick">invalidate</button>
     </div>
   </div>
 </template>
