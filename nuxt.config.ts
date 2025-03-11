c7fbe28758f34c0acf4224154aac998f4a0999b2
teleskop150750 <teleskop150750@gmail.com>
2025-03-11 11:45:04 +0300

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   compatibilityDate: "2024-11-01",
-  devtools: { enabled: true },
+  devtools: { enabled: false },
 
   experimental: {
     typedPages: true,
